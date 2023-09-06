4273d690ea3755b653f3d9dea9baebe25908875f
June222 <38900003+June222@users.noreply.github.com>
2023-09-06 13:55:16 +0900

    Make string data to variable
    - To prevent problems caused by typos.
    
    - To easily manage asset data.

---
+++ lib/data/string_data/string_data.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: constant_identifier_names
+
+const String jadu_image = "assets/jadu.jpg";
+const String coding_image = "assets/coding.jpg";
+const String service_image = "assets/service3.png";
+const String ai_image = "assets/ai.jpg";
+
+const String jw_image = "assets/junwoo.jpg";
+const String js_image = "assets/jisu.png";
+const String tk_image = "assets/taekyung.jpg";
+
+const String bg_text =
+    "철강 품질 관리 능력은 철강 회사의 가장 중요한 능력중 하나입니다. 이에 철강 표면의 불량을 검출에 필요한 다양한 인공모델을 비교하고 최적의 모델을 개발하였습니다.\n";
+const String pp_text =
+    "기획부터 모델개발, 서비스 환경구축과 배포까지 일련의 모든 과정을 직접 경험함으로써 \"교과목에서 배운 내용을 기반으로 실제 시스템 개발\"이라는 졸업과제의 의미를 온전히 수행 할 수 있도록 구성하였습니다.";
+
+const String service_intro_text_1 =
+    "철강 표면의 불량을 검출 하는 프로그램인 \"Steel See\"는 당신이 잠든 사이에도 여전히 잠들지 않고(Still See) 대량의 철강 이미지 데이터에서 불량을 찾는(Steel See) 서비스를 제공합니다.\n";
+const String service_intro_text_2 =
+    "여러분이 철강 이미지를 업로드하면 어플은 해당 이미지를 HTTP에 담아 ‘예측 결과값을 반환하는 API’를 호출해요. 그러면 API의 입력값으로 여러분의 이미지 데이터가 들어가고, 예측을 한 후, 결과값을 여러분에게 보여주게 돼요! 여러분은 결과값을 바탕으로 철강의 불량 여부, 위치 등을 파악할 수 있겠죠~!";
