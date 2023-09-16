ecb876d1716ca77d46590496c6c23b55e59d67c6
June222 <38900003+June222@users.noreply.github.com>
2023-09-16 21:12:57 +0900

    Make login page UI
    If press the button, circularIndicator come out for 1 sec and disappear.
    
    CircularIndicator is widget has pixel size only.
    
    After that replace with HomePage Screen.
    
    All of widget are made with fractional size depends on physical device.

---
+++ sfp_mobile_interface_flutter/lib/screens/login_page.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+import 'package:sfp_mobile_interface_flutter/data/data.dart';
+import 'package:sfp_mobile_interface_flutter/screens/home_page.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  static const String routeName = '/';
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool loadingBarOffstage = false;
+
+  void loginPressed() async {
+    setState(() {
+      loadingBarOffstage = true;
+    });
+    await Future.delayed(oneSec);
+    setState(() {
+      loadingBarOffstage = false;
+    });
+    Navigator.pushReplacementNamed(context, HomePage.routeName);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Spacer(),
+              const Text(
+                "SMART FACTORY",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+              ),
+              const Spacer(),
+              SizedBox(
+                width: 30,
+                height: 30,
+                child: Visibility(
+                  visible: loadingBarOffstage,
+                  child: const CircularProgressIndicator(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: height / 10,
+                width: width / 1.5,
+                child: ElevatedButton(
+                  onPressed: loginPressed,
+                  child: const Text("관리자 모드로 시작"),
+                ),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
