e7ce8329227973ce03e2cb3a9ab6e68a21c8409f
June222 <38900003+June222@users.noreply.github.com>
2023-09-19 19:33:33 +0900

    Make chart showing numbers all of label by date.
    X axis represent date, Y axis represent number of data.
    
    Each of color represent each of label.

--- sfp_mobile_interface_flutter/lib/screens/data_by_date_page.dart
+++ sfp_mobile_interface_flutter/lib/screens/data_by_date_page.dart
@@ -1,8 +1,8 @@
-
 // import 'dart:math';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class DateTimePage extends StatefulWidget {
   const DateTimePage({super.key});
@@ -18,6 +18,7 @@ class _DateTimePageState extends State<DateTimePage> {
 
   final int settingRate = 3;
   String? selectedValue = "All";
+  final bool _offstage = true;
 
   @override
   void initState() {
@@ -47,6 +48,7 @@ class _DateTimePageState extends State<DateTimePage> {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
 
     return GestureDetector(
       onTap: () => _focusNode.unfocus(),
@@ -146,52 +148,230 @@ class _DateTimePageState extends State<DateTimePage> {
                   ],
                 ),
                 Expanded(
-                  child: Scrollbar(
-                    child: ListView.builder(
-                      itemCount: 30,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  const Expanded(
-                                    child: Text(
-                                      "2023-09-14",
-                                      textAlign: TextAlign.center,
-                                    ),
+                  child: ListView.builder(
+                    itemCount: 1,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 15.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                const Expanded(
+                                  child: Text(
+                                    "2023-09-14",
+                                    textAlign: TextAlign.center,
                                   ),
-                                  Expanded(
-                                    child: ListTile(
-                                      title: const Text(
+                                ),
+                                Expanded(
+                                  child: Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      const Text(
                                         "150 / 150",
                                         textAlign: TextAlign.center,
                                       ),
-                                      trailing: IconButton(
-                                        onPressed: () {},
-                                        icon: const Icon(
-                                            Icons.arrow_downward_rounded),
+                                      Positioned(
+                                        left: 150,
+                                        right: 0,
+                                        child: IconButton(
+                                          onPressed: () {},
+                                          icon: const Icon(
+                                              Icons.arrow_downward_rounded),
+                                        ),
                                       ),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    child: const Text(
+                                      "자세히 보기",
+                                      textAlign: TextAlign.center,
                                     ),
                                   ),
-                                  Expanded(
-                                    child: TextButton(
-                                      onPressed: () {},
-                                      child: const Text(
-                                        "자세히 보기",
-                                        textAlign: TextAlign.center,
-                                      ),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              width: width,
+                              height: height / 2,
+                              decoration: BoxDecoration(
+                                color: Colors.red.shade100,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  // SfCartesianChart(
+                                  //   primaryXAxis: CategoryAxis(),
+                                  //   series: [
+                                  //     // ColumnSeries(
+                                  //     //   dataSource: <DataByClass>[
+                                  //     //     DataByClass("1", 80),
+                                  //     //     DataByClass("2", 50),
+                                  //     //     DataByClass("3", 15),
+                                  //     //     DataByClass("4", 5),
+                                  //     //   ],
+                                  //     //   xValueMapper: (datum, index) =>
+                                  //     //       datum.class_name,
+                                  //     //   yValueMapper: (datum, index) =>
+                                  //     //       datum.numbers,
+                                  //     // ),
+                                  //     LineSeries(
+                                  //       // markerSettings:
+                                  //       //     const TrackballMarkerSettings(
+                                  //       //   markerVisibility:
+                                  //       //       TrackballVisibilityMode.visible,
+                                  //       //   height: 100,
+                                  //       //   width: 20,
+                                  //       //   color: Colors.black,
+                                  //       //   shape: DataMarkerType.circle,
+                                  //       // ),
+                                  //       dataSource: <DataByClass>[
+                                  //         DataByClass("1", 80),
+                                  //         DataByClass("2", 50),
+                                  //         DataByClass("3", 15),
+                                  //         DataByClass("4", 5),
+                                  //       ],
+                                  //       xValueMapper: (datum, index) =>
+                                  //           datum.class_name,
+                                  //       yValueMapper: (datum, index) =>
+                                  //           datum.numbers,
+                                  //     ),
+                                  //   ],
+                                  //   // series: <LineSeries<DateByClass, String>>[
+                                  //   //   LineSeries<DateByClass, String>(
+                                  //   //     dataSource: <DateByClass>[
+                                  //   //       DateByClass("1", 80),
+                                  //   //       DateByClass("2", 50),
+                                  //   //       DateByClass("3", 15),
+                                  //   //       DateByClass("4", 5),
+                                  //   //     ],
+                                  //   //     xValueMapper: (datum, index) =>
+                                  //   //         datum.class_name,
+                                  //   //     yValueMapper: (datum, index) =>
+                                  //   //         datum.numbers,
+                                  //   //   )
+                                  //   // ],
+                                  // ),
+                                  // SfSparkLineChart(
+                                  //   trackball: const SparkChartTrackball(
+                                  //     activationMode:
+                                  //         SparkChartActivationMode.tap,
+                                  //   ),
+                                  //   marker: const SparkChartMarker(
+                                  //       displayMode:
+                                  //           SparkChartMarkerDisplayMode.all),
+                                  //   labelDisplayMode:
+                                  //       SparkChartLabelDisplayMode.all,
+                                  //   data: const [80, 50, 15, 5],
+                                  // ),
+                                  SfCartesianChart(
+                                    // plotAreaBorderWidth: 0.8,
+
+                                    zoomPanBehavior: ZoomPanBehavior(
+                                      enablePanning: true,
+                                      enableDoubleTapZooming: true,
+                                      maximumZoomLevel: 3,
                                     ),
+
+                                    primaryXAxis: CategoryAxis(),
+                                    // selectionType: SelectionType.series,
+                                    series: [
+                                      ColumnSeries(
+                                        dataSource: <DataByDate>[
+                                          DataByDate(80, 50, 15, 5, "1"),
+                                          DataByDate(80, 50, 15, 5, "2"),
+                                          DataByDate(50, 30, 5, 15, "3"),
+                                          DataByDate(50, 30, 5, 15, "4"),
+                                          DataByDate(50, 30, 5, 15, "5"),
+                                        ],
+                                        xValueMapper: (datum, index) =>
+                                            datum.date,
+                                        yValueMapper: (datum, index) =>
+                                            datum.class_one_num,
+                                      ),
+                                      ColumnSeries(
+                                        dataSource: <DataByDate>[
+                                          DataByDate(80, 50, 15, 5, "1"),
+                                          DataByDate(80, 50, 15, 5, "2"),
+                                          DataByDate(50, 30, 5, 15, "3"),
+                                          DataByDate(50, 30, 5, 15, "4"),
+                                          DataByDate(50, 30, 5, 15, "5"),
+                                        ],
+                                        xValueMapper: (datum, index) =>
+                                            datum.date,
+                                        yValueMapper: (datum, index) =>
+                                            datum.class_two_num,
+                                      ),
+                                      ColumnSeries(
+                                        dataSource: <DataByDate>[
+                                          DataByDate(80, 50, 15, 5, "1"),
+                                          DataByDate(80, 50, 15, 5, "2"),
+                                          DataByDate(50, 30, 5, 15, "3"),
+                                          DataByDate(50, 30, 5, 15, "4"),
+                                          DataByDate(50, 30, 5, 15, "5"),
+                                        ],
+                                        xValueMapper: (datum, index) =>
+                                            datum.date,
+                                        yValueMapper: (datum, index) =>
+                                            datum.class_three_num,
+                                      ),
+                                      ColumnSeries(
+                                        dataSource: <DataByDate>[
+                                          DataByDate(80, 50, 15, 5, "1"),
+                                          DataByDate(80, 50, 15, 5, "2"),
+                                          DataByDate(50, 30, 5, 15, "3"),
+                                          DataByDate(50, 30, 5, 15, "4"),
+                                          DataByDate(50, 30, 5, 15, "5"),
+                                        ],
+                                        xValueMapper: (datum, index) =>
+                                            datum.date,
+                                        yValueMapper: (datum, index) =>
+                                            datum.class_four_num,
+                                      ),
+                                    ],
                                   ),
+                                  // SfCartesianChart(
+                                  //   primaryXAxis: CategoryAxis(),
+                                  //   primaryYAxis: NumericAxis(),
+                                  //   series: <ChartSeries>[
+                                  //     // 첫 번째 시리즈
+                                  //     ColumnSeries<ChartData, String>(
+                                  //       dataSource: <ChartData>[
+                                  //         ChartData('Category A', 30),
+                                  //         ChartData('Category B', 50),
+                                  //         ChartData('Category C', 20),
+                                  //       ],
+                                  //       xValueMapper: (ChartData sales, _) =>
+                                  //           sales.category,
+                                  //       yValueMapper: (ChartData sales, _) =>
+                                  //           sales.value,
+                                  //       name: 'Series 1',
+                                  //     ),
+                                  //     // 두 번째 시리즈
+                                  //     ColumnSeries<ChartData, String>(
+                                  //       dataSource: <ChartData>[
+                                  //         ChartData('Category A', 40),
+                                  //         ChartData('Category B', 20),
+                                  //         ChartData('Category C', 60),
+                                  //       ],
+                                  //       xValueMapper: (ChartData sales, _) =>
+                                  //           sales.category,
+                                  //       yValueMapper: (ChartData sales, _) =>
+                                  //           sales.value,
+                                  //       name: 'Series 2',
+                                  //     ),
+                                  //   ],
+                                  // ),
                                 ],
                               ),
-                              // Container(width: width,child: Row(children: []),)
-                            ],
-                          ),
-                        );
-                      },
-                    ),
+                            )
+                          ],
+                        ),
+                      );
+                    },
                   ),
                 ),
               ],
@@ -201,4 +381,29 @@ class _DateTimePageState extends State<DateTimePage> {
       ),
     );
   }
+}
+
+class DataByClass {
+  final String class_name;
+  final double numbers;
+
+  DataByClass(this.class_name, this.numbers);
+}
+
+class DataByDate {
+  final String date;
+  final double class_one_num;
+  final double class_two_num;
+  final double class_three_num;
+  final double class_four_num;
+
+  DataByDate(this.class_one_num, this.class_two_num, this.class_three_num,
+      this.class_four_num, this.date);
+}
+
+class ChartData {
+  final String category;
+  final double value;
+
+  ChartData(this.category, this.value);
 }
