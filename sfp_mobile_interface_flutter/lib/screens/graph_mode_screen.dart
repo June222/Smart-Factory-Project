0fcd42c6774d90e748b140974e70a15835ac3446
June222 <38900003+June222@users.noreply.github.com>
2023-10-11 16:47:23 +0900

    Add GraphModeScreen
    Add screen to show graph of data

---
+++ sfp_mobile_interface_flutter/lib/screens/graph_mode_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GraphModeScreen extends StatelessWidget {
+  const GraphModeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
