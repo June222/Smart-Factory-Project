657c20e3174ae8479d8a465435e6c3ba9c4d1023
June222 <38900003+June222@users.noreply.github.com>
2023-10-17 21:12:40 +0900

    riverpod init

--- sfp_mobile_interface_flutter/lib/main.dart
+++ sfp_mobile_interface_flutter/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sfp_mobile_interface_flutter/data/data.dart';
 import 'package:sfp_mobile_interface_flutter/routers/main_page.dart';
 import 'package:sfp_mobile_interface_flutter/screens/login_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
