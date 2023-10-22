836d821d0cbf54d72ccf544e9ce6133b70f3161b
June222 <38900003+June222@users.noreply.github.com>
2023-10-22 14:59:23 +0900

    Add Entity with OneToMany Mapping
    mappedby에는 본인의 Table 이름을,
    
    연결된 Table에도 입력을 하고 싶으면 cascade = CascadeType.ALL을

---
+++ DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/domain/Steel.java
@@ -0,0 +1,74 @@
+package com.example.mysqljpaapi.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Setter
+@NoArgsConstructor
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table()
+@Entity
+@ToString
+public class Steel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "STEEL_ID")
+    private Integer id;
+
+    @Column(nullable = false, length = 20)
+    private String fileName;
+
+    @Column(nullable = false, length = 500)
+    private String originImgUrl;
+
+    @Column(nullable = false, length = 500)
+    private String detectedImgUrl;
+
+    @Column(nullable = false)
+    private boolean isNormal;
+
+
+    @OneToMany(mappedBy = "steel", orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<SteelLabel> labels = new ArrayList<SteelLabel>();
+
+    @CreatedDate
+    @Column()
+    private LocalDateTime localDateTime;
+
+    @Builder
+    public Steel(Integer id, String fileName, boolean isNormal,
+                 String originImgUrl, String detectedImgUrl,
+                 List<SteelLabel> labels,
+                 LocalDateTime localDateTime) {
+
+        this.id = id;
+        this.fileName = fileName;
+        this.originImgUrl = originImgUrl;
+        this.detectedImgUrl = detectedImgUrl;
+        this.labels = labels;
+        this.localDateTime = localDateTime;
+        this.isNormal = isNormal;
+    }
+    public void addLabel(SteelLabel label){
+        this.labels.add(label);
+    }
+
+    public List<Integer> getIntegerList(){
+        List<Integer> list = new ArrayList<>();
+        for(SteelLabel steelLabel: this.labels){
+            list.add(steelLabel.getLabel());
+        }
+        return list;
+    }
+
+    public void updateFileName(String newName){
+        this.fileName = newName;
+    }
+}
---
+++ DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/domain/SteelLabel.java
@@ -0,0 +1,42 @@
+package com.example.mysqljpaapi.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "steel_label_tb")
+public class SteelLabel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "LABEL_ID")
+    private int id;
+
+    private int label;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn()
+    private Steel steel;
+
+    @Builder
+    public SteelLabel(int id, int label, Steel steel){
+        this.id = id;
+        this.label = label;
+        this.steel = steel;
+    }
+
+    public static SteelLabel createLSteelLabel(Integer label, Steel steel){
+        return SteelLabel.builder()
+                .label(label)
+                .steel(steel)
+                .build();
+    }
+}
