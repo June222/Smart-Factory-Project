402ea6400deb01fd66f3d3f70d3af5b8ebef960b
June222 <38900003+June222@users.noreply.github.com>
2023-10-22 14:57:12 +0900

    Web 시작 프로그램

---
+++ DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/MysqlJpaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqljpaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlJpaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlJpaApiApplication.class, args);
+	}
+
+}
