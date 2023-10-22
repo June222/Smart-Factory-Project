c7f76b6eb66abdc0fd6be10664dfe7633de231f0
June222 <38900003+June222@users.noreply.github.com>
2023-10-22 15:34:50 +0900

    Add filter query.
    검색 조건에 따른 query문 작성
    1. 날짜로만 검색
    2. 날짜 + 정상 사진
    3. 날짜 + 불량 + label

---
+++ DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/domain/SteelRepository.java
@@ -0,0 +1,30 @@
+package com.example.mysqljpaapi.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public interface SteelRepository  extends JpaRepository<Steel, Integer> {
+
+    @Query("SELECT s FROM Steel s WHERE s.localDateTime BETWEEN :t1 AND :t2")
+    List<Steel> findSteelsByLocalDateTime(@Param("t1") LocalDateTime t1, @Param("t2") LocalDateTime t2);
+
+    @Query("SELECT s FROM Steel s WHERE s.isNormal = TRUE AND s.localDateTime BETWEEN :t1 AND :t2")
+    List<Steel> findNormalsByLocalDateTime(@Param("t1") LocalDateTime t1, @Param("t2")LocalDateTime t2);
+
+//    @Query(value = "SELECT s FROM Steel s JOIN ")
+
+
+//    @Query(value = "SELECT s FROM Steel s " +
+//            "WHERE s.isNormal = FALSE AND " +
+//            "s.localDateTime BETWEEN :t1 AND :t2 " +
+//            "" +
+//            "SELECT sl FROM SteelLabel sl " +
+//            "WHERE sl.label = :label")
+    @Query(value = "SELECT s, sl.label FROM Steel AS s JOIN SteelLabel AS sl ON s.id = sl.steel.id WHERE s.isNormal = FALSE AND s.localDateTime BETWEEN :t1 AND :t2 AND sl.label = :label")
+    List<Steel> findSteelsByLabelAndAndLocalDateTime(@Param("t1") LocalDateTime t1, @Param("t2") LocalDateTime t2, @Param("label") int label);
+}
