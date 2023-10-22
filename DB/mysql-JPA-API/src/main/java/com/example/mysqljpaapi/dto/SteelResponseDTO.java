0fcaab9c79b61ecd809514b25c804e68aabc2689
June222 <38900003+June222@users.noreply.github.com>
2023-10-22 15:37:07 +0900

    Add getResponseDto.
    요청할 때의 ResponseDTO 작성
    1.id
    2. 파일 이름
    3. 원본 이미지 url
    4. 검출 이미지 url
    5. 검출된 label list<int>
    6. 날짜(yyyy-MM-dd)
    7. 날짜(HH:mm:ss)

--- DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/dto/SteelResponseDTO.java
+++ DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/dto/SteelResponseDTO.java
@@ -30,36 +30,36 @@ public class SteelResponseDTO {
                     toStringFormat(steel.getLocalDateTime());
         }
     }
-//
-//    @ToString
-//    @Setter
-//    @Getter
-//    public static class GetRespDto{
-//        private int id;
-//        private String fileName;
-//        private String originImgUrl;
-//        private String detectedImgUrl;
-//        private List<Integer> labelList;
-//        private String date;
-//        private String second;
-//
-//        public GetRespDto(Steel steel){
-//            this.id = steel.getId();
-//            this.fileName = steel.getFileName();
-//            this.originImgUrl = steel.getOriginImgUrl();;
-//            this.detectedImgUrl = steel.getDetectedImgUrl();
-//            this.labelList = steel.getIntegerList();
-//            this.date = CustomDateUtil.dateToStringFormat(steel.getLocalDateTime());
-//            this.second = CustomDateUtil.secondToStringFormat(steel.getLocalDateTime());
-//        }
-//
-//        static public List<GetRespDto> getRespDtoList(List<Steel> listFound){
-//            List<GetRespDto> list = new ArrayList<>();
-//
-//            for (Steel steel : listFound) {
-//                list.add(new GetRespDto(steel));
-//            }
-//            return list;
-//        }
-//    }
+
+    @ToString
+    @Setter
+    @Getter
+    public static class GetRespDto{
+        private int id;
+        private String fileName;
+        private String originImgUrl;
+        private String detectedImgUrl;
+        private List<Integer> labelList;
+        private String date;
+        private String second;
+
+        public GetRespDto(Steel steel){
+            this.id = steel.getId();
+            this.fileName = steel.getFileName();
+            this.originImgUrl = steel.getOriginImgUrl();;
+            this.detectedImgUrl = steel.getDetectedImgUrl();
+            this.labelList = steel.getIntegerList();
+            this.date = CustomDateUtil.dateToStringFormat(steel.getLocalDateTime());
+            this.second = CustomDateUtil.secondToStringFormat(steel.getLocalDateTime());
+        }
+
+        static public List<GetRespDto> getRespDtoList(List<Steel> listFound){
+            List<GetRespDto> list = new ArrayList<>();
+
+            for (Steel steel : listFound) {
+                list.add(new GetRespDto(steel));
+            }
+            return list;
+        }
+    }
 }
