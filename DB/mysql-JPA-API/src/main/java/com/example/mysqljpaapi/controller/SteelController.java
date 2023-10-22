daa250a47d42c081abb7d71e5b5f35ff29c9e3c6
June222 <38900003+June222@users.noreply.github.com>
2023-10-22 15:26:34 +0900

    Add SteelController
    모델의 검출 결과를 전송 받아 잘 받았다는 메세지와 저장된 결과를 돌려주는 Method

---
+++ DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/controller/SteelController.java
@@ -0,0 +1,40 @@
+package com.example.mysqljpaapi.controller;
+
+import com.example.mysqljpaapi.dto.ResponseDTO;
+import com.example.mysqljpaapi.dto.SteelReqDto;
+import com.example.mysqljpaapi.dto.SteelReqDto.GetReqDto;
+import com.example.mysqljpaapi.dto.SteelReqDto.AddReqDto;
+import com.example.mysqljpaapi.dto.SteelResponseDTO;
+import com.example.mysqljpaapi.dto.SteelResponseDTO.GetRespDto;
+import com.example.mysqljpaapi.dto.SteelResponseDTO.AddRespDto;
+import com.example.mysqljpaapi.service.SteelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class SteelController {
+    private final SteelService steelService;
+
+    @ResponseBody
+    @PostMapping("/add")
+    public ResponseEntity<?> addData(@RequestBody @Valid SteelReqDto.AddReqDto requestDTO, BindingResult bindingResult){
+        AddRespDto addRespDto = steelService.addData(requestDTO);
+        return new ResponseEntity<>(new ResponseDTO<>(1,"성공 했습니다.", addRespDto), HttpStatus.CREATED);
+    }
+//
+//    @ResponseBody
+//    @GetMapping("/get")
+//    public ResponseEntity<?> getData(@RequestBody @Valid GetReqDto requestDTO, BindingResult bindingResult){
+//        List<GetRespDto> getRespDtoList = steelService.getData(requestDTO);
+//        return new ResponseEntity<>(new ResponseDTO<>(1, "성공 했습니다.", getRespDtoList), HttpStatus.ACCEPTED);
+//    }
+}
