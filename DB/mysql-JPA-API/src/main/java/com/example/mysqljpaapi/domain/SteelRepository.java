d36569a87e789750700a42eb02eef55ecba033b4
June222 <38900003+June222@users.noreply.github.com>
2023-10-25 00:03:12 +0900

    Rename Method.
    1. 결함만 찾는 것
    2. 결함에 Label까지 찾는 것.

--- DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/domain/SteelRepository.java
+++ DB/mysql-JPA-API/src/main/java/com/example/mysqljpaapi/domain/SteelRepository.java
@@ -16,15 +16,9 @@ public interface SteelRepository  extends JpaRepository<Steel, Integer> {
     @Query("SELECT s FROM Steel s WHERE s.isNormal = TRUE AND s.localDateTime BETWEEN :t1 AND :t2")
     List<Steel> findNormalsByLocalDateTime(@Param("t1") LocalDateTime t1, @Param("t2")LocalDateTime t2);
 
-//    @Query(value = "SELECT s FROM Steel s JOIN ")
+    @Query("SELECT s FROM Steel s WHERE s.isNormal = FALSE AND s.localDateTime BETWEEN :t1 AND :t2")
+    List<Steel> findDefectsByLocalDateTime(@Param("t1") LocalDateTime t1, @Param("t2")LocalDateTime t2);
 
-
-//    @Query(value = "SELECT s FROM Steel s " +
-//            "WHERE s.isNormal = FALSE AND " +
-//            "s.localDateTime BETWEEN :t1 AND :t2 " +
-//            "" +
-//            "SELECT sl FROM SteelLabel sl " +
-//            "WHERE sl.label = :label")
     @Query(value = "SELECT s, sl.label FROM Steel AS s JOIN SteelLabel AS sl ON s.id = sl.steel.id WHERE s.isNormal = FALSE AND s.localDateTime BETWEEN :t1 AND :t2 AND sl.label = :label")
-    List<Steel> findSteelsByLabelAndAndLocalDateTime(@Param("t1") LocalDateTime t1, @Param("t2") LocalDateTime t2, @Param("label") int label);
+    List<Steel> findDefectsByLabelAndLocalDateTime(@Param("t1") LocalDateTime t1, @Param("t2") LocalDateTime t2, @Param("label") int label);
 }
