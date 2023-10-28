e0c990dd65ee7b11b044fee6f799cc7789776018
June222 <38900003+June222@users.noreply.github.com>
2023-10-28 13:49:26 +0900

    Add showLoadingbar and 주석.
    showloadingbar를 메소드 앞뒤로 달고, 주석을 달았음.

--- sfp_mobile_interface_flutter/lib/resources/http_method.dart
+++ sfp_mobile_interface_flutter/lib/resources/http_method.dart
@@ -1,36 +1,60 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:flutter/material.dart';
 import 'package:sfp_mobile_interface_flutter/constants/classified.dart';
 import 'package:http/http.dart' as http;
 import 'package:sfp_mobile_interface_flutter/models/dash_board_image_model.dart';
 import 'package:sfp_mobile_interface_flutter/models/dash_board_request_model.dart';
 import 'package:sfp_mobile_interface_flutter/models/dash_board_response_model.dart';
+import 'package:sfp_mobile_interface_flutter/models/graph_bar_model.dart';
+import 'package:sfp_mobile_interface_flutter/models/graph_request_model.dart';
+import 'package:sfp_mobile_interface_flutter/models/graph_response_model.dart';
+import 'package:sfp_mobile_interface_flutter/setting/show_loading_bar.dart';
 
 class HttpMethod {
-  static Future<List<DashBoardImageModel>> getDataList(
-      DashBoardRequestModel model) async {
+  // http통신의 MIME 타입을 json형태로 설정
+  static const headers = {"Content-Type": "application/json"};
+
+  static Future<List<DashBoardImageModel>> getDashBoardDataList(
+      DashBoardRequestModel model, BuildContext context) async {
+    // loading 화면
+    showLoadingBar(context);
+
+    // jsonData List
     List<dynamic> dataList = [];
+    // flutter에서 사용할 클래스 List
     List<DashBoardImageModel> modelList = [];
 
-    final headers = {"Content-Type": "application/json"};
-    Uri uri = Uri.parse("http://$mysqlServerIPPort$specificURL");
+    Uri uri = Uri.parse("http://$mysqlServerIPPort$urlGetDashboard");
     try {
+      // http 통신
       var response = await http.post(
         uri,
         headers: headers,
         body: jsonEncode(model.toJson()),
       );
       log(response.body);
+
+      // 통신이 정상적으로 된 경우
       if (response.statusCode == 202) {
-        // log("success");
+        // json 데이터를 flutter에서 사용할 수 있게 변환
         var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
+
+        // 클래스로 변환
         final DashBoardResponseModel dashBoardResponseModel =
             DashBoardResponseModel.fromJson(jsonResponse);
 
         log(dashBoardResponseModel.data.toString());
+
+        // data에 담겨있는 list
         dataList = dashBoardResponseModel.data;
+
+        // list의 data들을 클래스로 변환
         for (var data in dataList) {
+          // modelList에 추가
           modelList.add(DashBoardImageModel.fromJson(data));
         }
       } else {
@@ -40,106 +64,58 @@ class HttpMethod {
       log("error");
       log(e.toString());
     }
+    Navigator.pop(context);
     return modelList;
   }
-  // // Flask API를 호출
-  // Future<DetectionImageModel?> transferToFlaskAPI(
-  //     final DetectionImageModel steelImageModel, BuildContext context) async {
-  //   // return 받을 변수 선언
-  //   DetectionImageModel? model;
-  //   showLoadingBar(context);
 
-  //   // http 통신 결과
-  //   var response = await http.post(
-  //     StaticData.flaskApiURL,
-  //     headers: StaticData.headers,
-  //     body: jsonEncode(DetectionImageModel.toJson(steelImageModel)),
-  //   );
+  static Future<List<GraphBarModel>> getGraphDataList(
+      GraphRequestModel model, BuildContext context) async {
+    showLoadingBar(context);
 
-  //   log("response comes");
-  //   Navigator.pop(context);
+    // jsonData List
+    List<dynamic> dataList = [];
+    // flutter에서 사용할 클래스 List
+    List<GraphBarModel> modelList = [];
 
-  //   // 통신이 정상적인 경우
-  //   if (response.statusCode == 200) {
-  //     // JSON 데이터를 managable 하게 변환
-  //     var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
-  //     model = DetectionImageModel.fromJson(jsonResponse);
-  //     log(response.body);
+    Uri uri = Uri.parse("http://$mysqlServerIPPort$urlGetGraph");
 
-  //     // 통신이 된 경우 결과 이미지를 띄움.
-  //     await showDialog(
-  //         context: context,
-  //         builder: (context) {
-  //           return Dialog(
-  //             child: Image.network(
-  //               model!.imageURL,
-  //               loadingBuilder: (context, child, loadingProgress) {
-  //                 // loading이 완료될 경우 원래 설정한 이미지를 렌더링
-  //                 if (loadingProgress == null) {
-  //                   return child;
-  //                 }
-  //                 // 아니면 로딩화면 렌더링
-  //                 return const Center(
-  //                   child: CircularProgressIndicator(color: mainColor),
-  //                 );
-  //               },
-  //               errorBuilder: (context, error, stackTrace) =>
-  //                   Text(error.toString()),
-  //             ),
-  //           );
-  //         });
-  //   } else {
-  //     showSnackBar("${response.statusCode}", context);
-  //     log("Response Error: ${response.statusCode}\n${response.body.toString()} ");
-  //   }
-  //   log("transfer Flask API() done.");
+    try {
+      // http 통신
+      var response = await http.post(
+        uri,
+        headers: headers,
+        body: jsonEncode(model.toJson()),
+      );
 
-  //   return model;
-  // }
+      log(response.body);
 
-  // // mysql(을 관리하고 있는 JPA)로 데이터 전송
-  // Future<MysqlResponseModel?> transferToMysqlServer(
-  //   final MysqlRequestModel requestModel,
-  //   BuildContext context,
-  // ) async {
-  //   log("init transfer to MysqlServer");
-  //   log(requestModel.toString());
+      // 통신이 정상적으로 된 경우
+      if (response.statusCode == 202) {
+        // json 데이터를 flutter에서 사용할 수 있게 변환
+        var jsonResponse = jsonDecode(response.body) as Map<String, dynamic>;
 
-  //   // return 받을 변수 선언
-  //   MysqlResponseModel? responseModel;
+        // 클래스로 변환
+        final GraphResponseModel graphResponseModel =
+            GraphResponseModel.fromJson(jsonResponse);
 
-  //   showLoadingBar(context);
-  //   try {
-  //     // http 통신 결과
-  //     var response = await http.post(
-  //       StaticData.mysqlServerURL,
-  //       headers: StaticData.headers,
-  //       body: jsonEncode(requestModel.toJson()),
-  //     );
+        log(graphResponseModel.data.toString());
 
-  //     log(response.body);
+        // data에 담겨있는 list
+        dataList = graphResponseModel.data;
 
-  //     // 통신이 정상적인 경우
-  //     if (response.statusCode == 201) {
-  //       // JSON 데이터를 managable 하게 변환
-  //       var jsonReponse = jsonDecode(response.body) as Map<String, dynamic>;
-  //       responseModel = MysqlResponseModel.fromJson(jsonReponse);
-
-  //       log(responseModel.toString());
-
-  //       // 전송이 된 경우 알림 띄움.
-  //       await showTextDialog("mysql 서버에 등록을 완료했습니다.", context);
-  //     } else {
-  //       showSnackBar("${response.statusCode}", context);
-  //       log("Response Error: ${response.statusCode}\n${response.body.toString()} ");
-  //     }
-  //   } catch (e) {
-  //     log(e.toString());
-  //   }
-
-  //   // loading bar 제거
-  //   Navigator.pop(context);
-
-  //   return responseModel;
-  // }
+        // list의 data들을 클래스로 변환
+        for (var data in dataList) {
+          // modelList에 추가
+          modelList.add(GraphBarModel.fromJson(data));
+        }
+      } else {
+        log(response.statusCode.toString());
+      }
+    } catch (e) {
+      log("error");
+      log(e.toString());
+    }
+    Navigator.pop(context);
+    return modelList;
+  }
 }
